954000b19f777bbd2e43dceca14ecab0a7d48a8f
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2020-05-31 12:07:45 -0700

    Created multiple loading bars for practice

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
 const loadingBarItemEl = document.querySelector(".loading-bar__item");
+const loadingBar2ItemEl = document.querySelector("#loading-bar2");
+const loadingBar3ItemEl = document.querySelector("#loading-bar3");
+const button1El = document.querySelector(".first__button")
 
+// *DISREGARD BUTTONS. ILL GET BACK TO IT WHEN I KNOW MORE :D
+
+// function buttonClick() {
+//   let width = 1;
+// button1El.addEventListener("click", function(
+//   let loadingBar = setInterval(function(){
+//     if (width >= 100) {
+//       clearInterval(loadingBar)
+//     } else {
+//       width++;
+//       loadingBarItemEl.style.width = width + "%"
+//     }
+//   }, 100)
+// )}
+
+
+
+// Loading Bar # 1
 let width = 1;
-
-let loadingBar = setInterval(function() {
-  if (width >= 100) {
-    clearInterval(loadingBar);
+let loadingBar = setInterval(function(){
+  if (loadingBarItemEl.style.width >= 100) {
+    clearInterval(loadingBar)
   } else {
     width++;
     loadingBarItemEl.style.width = width + "%"
   }
-})
+}, 100)
 
 
+// Loading Bar # 2
+let loadingbar2 = setInterval(function(){
+  if (loadingBar2ItemEl.style.width >= 100) {
+    clearInterval(loadingBar)
+  } else {
+    width++;
+    loadingBar2ItemEl.style.width = width + "%"
+  }
+}, 5000)
 
+// Loading Bar # 3
+let loadingBar3 = setInterval(function() {
+  if (loadingBar3ItemEl.style.width >= 100) {
+    clearInterval(loadingBar)
+  } else {
+    width++;
+    loadingBar3ItemEl.style.width = width + "%"
+  }
+}, 300)
 
-
-
-
-
-
-
-
-
-// let count = 0;
-// let incrementor = setInterval(function() {
-//   if (count === 10) {
-//     clearInterval(incrementor)
-//   } else {
-//   count++;
-//   console.log(count)
-//   }
-// }, 1000)
