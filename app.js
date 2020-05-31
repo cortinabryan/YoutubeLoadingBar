8f1281648d555bfb3a9477576ac30368c4622f33
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2020-05-31 10:54:31 -0700

    Added more loading bars for practice

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const loadingBarItemEl = document.querySelector(".loading-bar__item");
 
 let width = 1;
-let loadingBar = setInterval(function () {
+
+let loadingBar = setInterval(function() {
   if (width >= 100) {
-    clearInterval(loadingBar)
+    clearInterval(loadingBar);
   } else {
     width++;
-    loadingBarItemEl.style.width = width + "%";
+    loadingBarItemEl.style.width = width + "%"
   }
-}, 10)
-
-
-
+})
 
 
 
